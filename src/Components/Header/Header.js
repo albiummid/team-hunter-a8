649b3a17bc7fb951e5373e3c8be2b93d20e6480a
albiummid <albi.ummid@gmail.com>
2021-03-11 20:50:06 +0600

    completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import "./Header.css"
+import logo from '../../Photo/team-hunter.png';
+import menuIcon from '../../Photo/menu.png';
 const Header = (props) => {
     return (
-        <div className="header-container">
-    
+        <div className="header">
+                <div className="navbar">
+                <img src={logo} className="logo" alt="" />
+                <nav>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Teams</a></li>
+                        <li><a href="">Materials</a></li>
+                        <li><a href="">About Us</a></li>
+                    </ul>
+                </nav>
+                <img src={menuIcon} className="menu-icon" alt="" />
+            </div>
+            <div className="header-container">
+        
                 {
                     props.children
             }
         </div>
+        </div>
+        
     );
 };
 
